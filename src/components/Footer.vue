ddfc4a471662876451a3959984a8d05245dda81a
Shaon <helloshaon@protonmail.com>
2025-07-21 13:30:31 +0600

    Add footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,6 +1,28 @@
 <template>
-  <p class="text-center py-3 mt-5">
-    <span>Developed by <a href="https://github.com/rashadulshaon" target="_blank"
-        class="text-primary text-decoration-none">Rashadul Shaon</a></span>
+  <section id="contact" class="container my-5 py-5">
+    <div class="col-lg-7 mx-auto text-center mb-4">
+      <h3 class="mb-3 display">Let’s craft
+        brilliance
+        together!</h3>
+      <p class="mb-4">I am always open to discussing new projects, creative ideas, or opportunities to
+        be part
+        of your visions. For business related queries, feel
+        free to reach out.</p>
+      <div class="d-md-inline mb-3 me-4">
+        <a class="text-decoration-none text-dark" href="mailto:codershaon@gmail.com">
+          <i class="ri-mail-line me-2 rounded-circle border p-2"></i>
+          <span>codershaon@gmail.com</span>
+        </a>
+      </div>
+      <div class="d-md-inline mb-4">
+        <a class="text-decoration-none text-dark" href="tel:+8801708777376">
+          <i class="ri-phone-line me-2 rounded-circle border p-2"></i>
+          <span>+880 1708 777 376</span>
+        </a>
+      </div>
+    </div>
+  </section>
+  <p class="text-center py-3 mb-5">
+    Sincerely, <strong class="text-primary">Rashadul Shaon</strong>
   </p>
 </template>
