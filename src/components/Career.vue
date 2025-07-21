d7b3a5f719b1e0f4378b909f14321462a586841a
Shaon <helloshaon@protonmail.com>
2025-07-21 15:49:19 +0600

    Add zoom on hover

--- src/components/Career.vue
+++ src/components/Career.vue
@@ -6,28 +6,28 @@
         software engineer and architect for several years, and I have gained a lot of experience in building
         applications that are easy to maintain and extend.</p>
     </div>
-    <div class="row bg-secondary p-3 rounded mb-3">
+    <div class="row bg-secondary p-3 rounded mb-3 hover-zoom">
       <div class="col-6 col-lg-3 col-xl-5"><i class="ri-server-line me-2"></i>Zorn Technologies</div>
       <div class="col-6 col-lg-3 col-xl-3 text-end text-lg-start">Software Architect</div>
       <div class="col-6 col-lg-3 col-xl-2 text-muted">Mar 2018 - Jan 2021</div>
       <div class="col-6 col-lg-3 col-xl-2 text-end text-muted">Jashore, Bangladesh</div>
     </div>
 
-    <div class="row bg-secondary p-3 rounded mb-3">
+    <div class="row bg-secondary p-3 rounded mb-3 hover-zoom">
       <div class="col-6 col-lg-3 col-xl-5"><i class="ri-shield-star-line me-2"></i>Macroman Solution</div>
       <div class="col-6 col-lg-3 col-xl-3 text-end text-lg-start">Back-End Developer</div>
       <div class="col-6 col-lg-3 col-xl-2 text-muted">Feb 2022- Dec 2023</div>
       <div class="col-6 col-lg-3 col-xl-2 text-end text-muted">Rajshahi, Bangladesh</div>
     </div>
 
-    <div class="row bg-secondary p-3 rounded mb-3">
+    <div class="row bg-secondary p-3 rounded mb-3 hover-zoom">
       <div class="col-6 col-lg-3 col-xl-5"><i class="ri-code-box-line me-2"></i>Razinsoft</div>
       <div class="col-6 col-lg-3 col-xl-3 text-end text-lg-start">Full-Stack Developer</div>
       <div class="col-6 col-lg-3 col-xl-2 text-muted">Jan 2024 - Sep 2024</div>
       <div class="col-6 col-lg-3 col-xl-2 text-end text-muted">Dhaka, Bangladesh</div>
     </div>
 
-    <div class="row bg-secondary p-3 rounded mb-3">
+    <div class="row bg-secondary p-3 rounded mb-3 hover-zoom">
       <div class="col-6 col-lg-3 col-xl-5"><i class="ri-briefcase-3-line me-2"></i>Insurasoft</div>
       <div class="col-6 col-lg-3 col-xl-3 text-end text-lg-start">Principal Engineer</div>
       <div class="col-6 col-lg-3 col-xl-2 text-muted">Oct 2024 - <span class="text-primary">Present</span></div>
@@ -35,3 +35,13 @@
     </div>
   </section>
 </template>
+
+<style scoped>
+.hover-zoom {
+  transition: transform 150ms ease;
+}
+
+.hover-zoom:hover {
+  transform: scale(1.02);
+}
+</style>
